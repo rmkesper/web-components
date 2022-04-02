b67cc4347f51cb8f068019086505095bf68083fc
rmkesper <82117687+rmkesper@users.noreply.github.com>
2022-04-02 17:51:30 +0200

    Add files via upload

---
+++ rippleeffect.js
@@ -0,0 +1,215 @@
+/*
+ * (c) RM//Kesper
+ * MIT License
+ */
+class RippleEffect {
+	rippleName = 'ripple' // provide additional properties like "ripple[<color>|<duration>|<opacityStart>|<scaleRippleEnd>]" in class name
+	rippleNameDisableParentEffect = 'unripple' // if this class is contained in a child element, the parent does not ripple
+	rippleCSSName = 'ani_expand'
+	rippleStyleHolder = 'rpl-frame'
+	rippleAnimationsName = 'bg_ani_rpl';
+	rippleCol = 'black'
+	rippleScaleStart = 0
+	rippleScaleEnd = 3
+	rippleOpacityStart = 0.2
+	rippleOpacityEnd = 0
+	rippleAniDuration = 1.35 //in s!
+	add() {
+		let self = this;
+		let _ = new RippleEffect;
+		let html = document.querySelector("html")
+		let htmlCls = html.getAttribute("class")
+		let rpladd = "rpleff-add"
+		if(html && (!htmlCls || (htmlCls && htmlCls.indexOf(rpladd)<0)) ) {
+			(new MutationObserver(function(mutations) {
+				mutations.forEach(function(mutation) {
+					(new RippleEffect()).add()
+				});
+			})).observe(document.body, { attributes: true, childList: true, characterData: true })
+			if(!htmlCls) html.setAttribute("class", rpladd)
+			else html.setAttribute("class", htmlCls + ' ' + rpladd)
+			
+		}
+		// update the dom style with the ripple keyframe & css pseudo element
+		if(!document.getElementById(_.rippleStyleHolder)){
+			let head = document.head || document.getElementsByTagName("head")[0]
+			let style = document.createElement("style")
+			let css = ''
+			css += `@keyframes ${_.rippleAnimationsName}
+			{0% {
+			opacity:var(--rpl-opacity-start);
+			transform: translate3d(0, 0, 0);
+			background-size: calc(var(--rpl-max) * var(--rpl-scl-start)) calc(var(--rpl-max) * var(--rpl-scl-start));
+			background-position-x: calc(var(--rpl-dx) - var(--rpl-max) * var(--rpl-scl-start) / 2);
+			background-position-y: calc(var(--rpl-dy) - var(--rpl-max) * var(--rpl-scl-start) / 2);
+			}
+			50% {
+			opacity: calc( ( var(--rpl-opacity-start) - var(--rpl-opacity-end) ) / 2 );
+			transform: translate3d(0, 0, 0);
+			background-size: calc(var(--rpl-max) * ( ( var(--rpl-scl-end) - var(--rpl-scl-start) ) / 2 ) ) calc(var(--rpl-max) * ( ( var(--rpl-scl-end) - var(--rpl-scl-start) ) / 2 ));
+			background-position-x: calc(var(--rpl-dx) - var(--rpl-max) * ( ( var(--rpl-scl-end) - var(--rpl-scl-start) ) / 2 ) / 2);
+			background-position-y: calc(var(--rpl-dy) - var(--rpl-max) * ( ( var(--rpl-scl-end) - var(--rpl-scl-start) ) / 2 ) / 2);
+			}
+			100% {
+			opacity:var(--rpl-opacity-end);
+			transform: translate3d(0, 0, 0);
+			background-size: calc(var(--rpl-max) * var(--rpl-scl-end)) calc(var(--rpl-max) * var(--rpl-scl-end));
+			background-position-x: calc(var(--rpl-dx) - var(--rpl-max) * var(--rpl-scl-end) / 2);
+			background-position-y: calc(var(--rpl-dy) - var(--rpl-max) * var(--rpl-scl-end) / 2);
+			}}
+			.${_.rippleCSSName}::before{
+			content: "";
+			z-index: -1;
+			position: absolute;
+			transform-origin: center;
+			backface-visibility: hidden;
+			transform: translate3d(0, 0, 0);
+			background-repeat: no-repeat;
+			background-color: transparent;
+			width: 100%;
+			height: 100%;
+			top: calc(50% - var(--rpl-top));
+			left: calc(50% - var(--rpl-left));
+			background-size: calc(var(--rpl-max) * var(--rpl-scl-start)) calc(var(--rpl-max) * var(--rpl-scl-start));
+			background-position-x: calc(var(--rpl-dx) - var(--rpl-max) * var(--rpl-scl-start) / 2);
+			background-position-y: calc(var(--rpl-dy) - var(--rpl-max) * var(--rpl-scl-start) / 2);
+			border-top-left-radius:var(--rpl-border-tl-radius);
+			border-top-right-radius:var(--rpl-border-tr-radius);
+			border-bottom-left-radius:var(--rpl-border-bl-radius);
+			border-bottom-right-radius:var(--rpl-border-br-radius);
+			background-image: var(--rpl-url);
+			animation: ${_.rippleAnimationsName} var(--rpl-ani-duration) var(--rpl-ani-state) forwards;
+			}`
+			head.appendChild(style)
+			style.setAttribute("id", _.rippleStyleHolder);
+			style.appendChild(document.createTextNode(css));
+		}
+		// catch the ripple element with a wildcard filter (in case some colors are provided, working with tailwind too...)
+		document.querySelectorAll(`[class*='${_.rippleName}']`).forEach( itm => {
+			if(!itm.classList.contains(rpladd)) {
+				// call the .add() method on mousedown / touchstart, run the event on mouseup / touchend
+				if(document['addEventListener']) {
+					itm.addEventListener(`mousedown`, self.create);
+					itm.addEventListener(`touchstart`, self.create);
+					itm.addEventListener(`mouseup`, self.end);
+					itm.addEventListener(`touchend`, self.end);
+				} else {
+					(itm).attachEvent(`onmousedown`, self.create);
+					(itm).attachEvent(`ontouchstart`, self.create);
+					(itm).attachEvent(`onmouseup`, self.end);
+					(itm).attachEvent(`ontouchend`, self.end);
+				}
+				itm.classList.add(rpladd)
+			}
+		})
+	}
+	getP(p) {
+		let _ = new RippleEffect;
+		function getCls(e){
+				let c = e.className ?? ""
+				return c.baseVal ?? c
+		}
+		while( getCls(p).indexOf(_.rippleName)<0 && p.tagName.toLowerCase() !== "body" ) {
+			if(getCls(p).indexOf(_.rippleNameDisableParentEffect)>=0) return
+			p = p.parentElement
+		}
+		return p
+	}
+	end(event) {
+		let _ = new RippleEffect;
+		let p = _.getP(event.target)
+		if(p.className.indexOf(_.rippleName)<0) return;
+		let id = p.getAttribute("data-rpl-id")
+		let rippleDuration = parseInt(p.style.getPropertyValue('--rpl-ani-duration'))
+		p.setAttribute("data-rpl-end", "true")
+		if(p.style.getPropertyValue('--rpl-ani-state') === "running") return
+		p.style.setProperty('--rpl-ani-state', 'running')
+		setTimeout( () => {
+			if(document.getElementById(id)) 
+				document.getElementById(id).remove()
+		}, rippleDuration * 1000 / 2 )
+	}
+	create(event){
+		let _ = new RippleEffect;
+		let p = _.getP(event.target)
+		if(p.className.indexOf(_.rippleName)<0) return;
+		let id = "_" + (new Date()).getTime() + Math.floor(Math.random()*9999)
+		// clear last animation
+		p.style.setProperty('--rpl-scl-start', 0)
+		// check if selected ripple parent is at not the 'static' position value
+		if(window.getComputedStyle(p).getPropertyValue('position').trim().toLocaleLowerCase() === 'static')
+			p.style.setProperty('position', 'relative')
+		let rippleCol = _.rippleCol;
+		let opacityStart = _.rippleOpacityStart;
+		let rippleDuration = _.rippleAniDuration;
+		let rippleScaleEnd = _.rippleScaleEnd;
+		// check if attributes were passed to current button
+		for(let cls of p.className.split(" ")) {
+			if(cls.indexOf(_.rippleName)>=0){
+				// check if an array is provided
+				if(cls.split('[').length>1){
+					// parse the array by split (prevent crash by wrong namings)
+					let clsStr = cls.split(_.rippleName+'[')[1].split(']').join('')	
+					let attrs = ( clsStr ).split('|')
+					rippleCol = attrs[0].trim();
+					rippleDuration = attrs.length>1 ? (attrs[1].trim() !== 'null' ? attrs[1].trim() : rippleDuration) : rippleDuration
+					opacityStart = attrs.length>2 ? (attrs[2].trim() !== 'null' ? attrs[2].trim() : opacityStart) : opacityStart
+					rippleScaleEnd = attrs.length>3 ? (attrs[3].trim() !== 'null' ? attrs[3].trim() : rippleScaleEnd) : rippleScaleEnd
+					break;
+				}
+			}
+		}
+		// prepare the bg svg element
+		let dataURL = 'data:image/svg+xml;base64,' +
+			btoa(`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 11 11">` +
+				`<path fill="${rippleCol}" d="M10,5.5C10,7.9853,7.9853,10,5.5,10S1,7.9853,1,5.5S3.0147,1,5.5,1S10,3.0147,10,5.5z"/>` +
+				`</svg>`)
+		let clientX = event.clientX ?? event.changedTouches[0].clientX
+		let clientY = event.clientY ?? event.changedTouches[0].clientY
+		let prgt = p.getBoundingClientRect();
+		let w = prgt.width;
+		let h = prgt.height;
+		let dx = clientX - prgt.x
+		let dy = clientY - prgt.y
+		let max = Math.max(w, h) * 2.5
+		p.setAttribute("data-rpl-id", id)
+		p.setAttribute("data-rpl-end", "false")
+		p.style.setProperty('--rpl-border-tl-radius', window.getComputedStyle(p).borderTopLeftRadius)
+		p.style.setProperty('--rpl-border-tr-radius', window.getComputedStyle(p).borderTopRightRadius)
+		p.style.setProperty('--rpl-border-bl-radius', window.getComputedStyle(p).borderBottomLeftRadius)
+		p.style.setProperty('--rpl-border-br-radius', window.getComputedStyle(p).borderBottomRightRadius)
+		p.style.setProperty('--rpl-opacity-start', opacityStart)
+		p.style.setProperty('--rpl-opacity-end', _.rippleOpacityEnd)
+		p.style.setProperty('--rpl-ani-duration', `${rippleDuration}s`)
+		p.style.setProperty('--rpl-url', `url(${dataURL}`)
+		p.style.setProperty('--rpl-dx', `${dx}px`)
+		p.style.setProperty('--rpl-dy', `${dy}px`)
+		p.style.setProperty('--rpl-max', `${max}px`)
+		p.style.setProperty('--rpl-scl-start', _.rippleScaleStart)
+		p.style.setProperty('--rpl-scl-end', rippleScaleEnd)
+		p.style.setProperty('--rpl-ani-state', 'running')
+		p.style.setProperty('--rpl-top', `${h/2}px`)
+		p.style.setProperty('--rpl-left', `${w/2}px`)
+		// now insert an element at first position into parent and append the ripple css to it
+		let n = document.createElement("SPAN")
+		if(p.firstElementChild){
+			p.insertBefore(n, p.firstElementChild);
+		} else p.appendChild(n)
+		n.setAttribute("class", _.rippleCSSName)
+		n.setAttribute("id", id)
+		// detect if we did not yet receive the end trigger of the animation, then pause ani
+		setTimeout( () => {
+			if(p.getAttribute("data-rpl-end") === "false") {
+				p.style.setProperty('--rpl-ani-state', 'paused')
+			}
+		}, rippleDuration * 1000 / 2)
+		// define the ripple end size and check if we can remove the ripple (end received)
+		setTimeout( () => {
+			p.style.setProperty('--rpl-scl-start', 0)
+			if((p.getAttribute("data-rpl-end") === "true")) {
+				if(document.getElementById(id)) 
+					n.remove()
+			}
+		}, rippleDuration * 1000 )
+	}
+}
---
+++ tooltip.js
@@ -0,0 +1,106 @@
+/*
+ * (c) RM//Kesper
+ * MIT License
+ */
+class ToolTip {
+	css = "bg-black/80 dark:bg-white/90 text-white dark:text-black text-xs px-2 py-1 rounded-sm shadow truncate"
+	add(css = this.css) {
+		this.css = css
+		let self = this
+		let html = document.querySelector("html")
+		let htmlCls = html.getAttribute("class")
+		let tipadd = "tltip-add"
+		if(html && (!htmlCls || (htmlCls && htmlCls.indexOf(tipadd)<0)) ) {
+			document.addEventListener("mousedown", () => (new ToolTip()).add())
+			document.addEventListener("touchstart", () => (new ToolTip()).add())
+			if(!htmlCls) html.setAttribute("class", tipadd)
+			else html.setAttribute("class", htmlCls + ' ' + tipadd)
+		}
+		let ts = document.querySelectorAll('[data-tip]')
+		for(let t of ts) {
+			if(!t.classList.contains("tooltip-add")) {
+				t.classList.add("tooltip-add")
+				t.setAttribute("data-tip-id", "_" + (new Date()).getTime() + Math.floor(Math.random()*9999))
+				t.title=""
+				t.addEventListener("mouseenter", function(){ self.create.call(self) }, false )
+				t.addEventListener("touchstart", function(){ self.create.call(self) }, false )
+				t.addEventListener("mouseleave", self.rm)
+				t.addEventListener("touchend", self.rm)
+			}
+		}
+	}
+	rm() {
+		let e = event.target
+		if(document.getElementById(e.getAttribute("data-tip-id"))) {
+			document.getElementById(e.getAttribute("data-tip-id")).remove()
+		}
+	}
+	create() {
+		let _css = this.css
+		let e = event.target
+		let attr = e.getAttribute("data-tip")
+		let txt = attr.charAt(0) === "#" ? document.getElementById(attr.substr(1))?.innerHTML : attr
+		let id = e.getAttribute("data-tip-id")
+		if(document.getElementById(id)) {
+			document.getElementById(id).remove()
+		}
+		if(e.getAttribute("data-tip-css")) _css = e.getAttribute("data-tip-css")
+		let pos = e.getAttribute("data-tip-pos") ?? "top"
+		let off = parseInt( e.getAttribute("data-tip-offset") ?? "10" )
+		let dur = parseInt( e.getAttribute("data-tip-timeout") ?? "150" )
+		let x = e.getBoundingClientRect().x
+		let y = e.getBoundingClientRect().y
+		let w = e.getBoundingClientRect().width
+		let h = e.getBoundingClientRect().height
+		let n = document.createElement("DIV")
+		document.querySelector("HTML").appendChild(n)
+		n.setAttribute("id", id)
+		if(txt.indexOf("class")<0 && txt.indexOf("style")<0) {
+			n.setAttribute("class", _css + (txt.indexOf("<")<0 ? ' flex items-center ' : ''))
+		}
+		n.classList.add("fixed")
+		n.classList.add("opacity-0")
+		n.classList.add("transition-opacity")
+		n.innerHTML = txt
+		n.style.setProperty("top", 0 + "px")
+		n.style.setProperty("left", 0 + "px")
+		n.style.setProperty("width", "fit-content")
+		let nw = n.getBoundingClientRect().width
+		let nh = n.getBoundingClientRect().height
+		let _x = x
+		let _y = y
+		if(pos === "top") {
+			_x = x - (nw - w) / 2
+			if( _x <= 0) _x = off
+			if( _x+nw >= window.innerWidth) _x = window.innerWidth - 10 - nw
+			_y = y - nh - off
+			if( _y <= 0) _y = y+h+off
+		}
+		else if(pos === "bottom") {
+			_x = x - (nw - w) / 2
+			if( _x <= 0) _x = off
+			if( _x+nw+off >= window.innerWidth) _x = window.innerWidth - 10 - nw
+			_y = y+h+off
+			if( _y+nh+off > window.innerHeight) _y = y - nh - off
+		}
+		else if(pos === "left") {
+			_x = x - nw - off
+			if( _x <= 0) _x = x+w+off
+			_y = y - (nh - h) / 2
+			if( _y <= 0) _y = off
+			if( _y+nh+off > window.innerHeight) _y = window.innerHeight - 10 - nh
+		}
+		else if(pos === "right") {
+			_x = x+w+off
+			if( _x+nw+off >= window.innerWidth) _x = x - nw - off
+			_y = y - (nh - h) / 2
+			if( _y <= 0) _y = off
+			if( _y+nh+off > window.innerHeight) _y = window.innerHeight - 10 - nh
+		}
+		n.style.setProperty("top", _y + "px")
+		n.style.setProperty("left", _x + "px")
+		setTimeout( () => {
+			if(document.getElementById(id)) document.getElementById(id).classList.remove("opacity-0")
+		}, dur)
+	}
+}
